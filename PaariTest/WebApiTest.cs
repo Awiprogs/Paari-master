1b24890a217604f5efc2aa28708bbdf697f2bf71
Rafal Wiglenda <rafal.wiglenda@gmail.com>
2018-03-18 19:31:06 +0100

    Add project files.

---
+++ PaariTest/WebApiTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaariTest
+{
+    [TestClass]
+    public class WebApiTest
+    {
+        private HttpClient _client;
+
+        public WebApiTest()
+        {
+            string url = ConfigurationManager.AppSettings["WebApiUrl"];
+            _client = new HttpClient {BaseAddress = new Uri(url)};
+
+            ProductController apiController = new ProductController();
+        }
+
+        [TestMethod]
+        public void WebApiConnectionTestTest()
+        {
+            //_client.
+        }
+
+        [TestMethod]
+        public void GetAllTest()
+        {
+        }
+    }
+}
---
+++ src/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1b5b9da5-347c-4539-89d6-610b26674b86")]
